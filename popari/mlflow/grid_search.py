79f51e6896a1be74e8b7e0a44684d7706e0e02d2
Shahul Alam <alam.shahul@gmail.com>
2023-08-01 17:27:56 -0400

    Added some MLflow functionality.

---
+++ popari/mlflow/grid_search.py
@@ -0,0 +1,160 @@
+from concurrent.futures import ThreadPoolExecutor
+import argparse
+import json
+import itertools
+import toml
+
+import numpy as np
+import torch
+
+import mlflow
+import mlflow.tracking
+import mlflow.projects
+from mlflow.tracking import MlflowClient
+
+from pathlib import Path
+from popari.mlflow.util import generate_mlproject_file
+
+def run():
+    parser = argparse.ArgumentParser(description='Run Popari hyperparameter grid search.')
+    parser.add_argument('--configuration_filepath', type=str, required=True, help="Path to configuration file.")
+    parser.add_argument('--output_path', type=str, default=".", help="Where to output MLflow results.")
+
+    args = parser.parse_args()
+
+    with open(args.configuration_filepath, "rb") as f:
+        configuration = toml.load(f)
+
+    output_path = Path(args.output_path)
+    generate_mlproject_file("mouse_heart_development", output_path / "MLproject")
+
+    hyperparameter_names = ('K', 'lambda_Sigma_x_inv', 'lambda_Sigma_bar', 'hierarchical_levels', 'binning_downsample_rate')
+    spatial_affinity_groups = configuration['spatial_affinity_groups'] if 'spatial_affinity_groups' in configuration else "null"
+    nmf_preiterations = configuration['hyperparameters']['nmf_preiterations']
+    spatial_preiterations = configuration['hyperparameters']['spatial_preiterations']
+    num_iterations = configuration['hyperparameters']['num_iterations']
+
+    max_p = configuration['max_p']
+
+    tracking_client = MlflowClient()
+
+    device_status = [False for _ in range(max_p)]
+    def generate_evaluate_function(parent_run, nmf_preiterations, spatial_preiterations, num_iterations, experiment_id, null_nll=0):
+        """Generates function to evaluate Popari.
+
+        """
+
+        def evaluate(hyperparams):
+            """Start parallel Popari run and track progress.
+
+
+            """
+
+            # (K, lambda_Sigma_x_inv, lambda_M, lambda_Sigma_bar) = hyperparams
+           
+            device = device_status.index(False)
+            device_status[device] = True 
+            child_run = tracking_client.create_run(experiment_id, tags={"mlflow.parentRunId": parent_run.info.run_id})
+            # with mlflow.start_run(nested=True) as child_run:
+            p = mlflow.projects.run(
+                run_id=child_run.info.run_id,
+                uri=output_path,
+                entry_point="train_debug",
+                hyperparameters={
+                    **{hyperhyperparameter_name: hyperparams[hyperhyperparameter_name] for hyperhyperparameter_name in hyperhyperparameter_names},
+                    "spatial_affinity_mode": "shared lookup" if lambda_Sigma_bar == 0 else "differential lookup",
+                    "spatial_affinity_groups": "null" if lambda_Sigma_bar == 0 else spatial_affinity_groups,
+                    "dataset_path": args.dataset_path,
+                    "output_path": args.output_path,
+                    "num_iterations": num_iterations,
+                    "spatial_preiterations": spatial_preiterations,
+                    "dtype": "float64",
+                    "torch_device": f"cuda:{device}",
+                    "initial_device": f"cuda:{device}",
+                    "nmf_preiterations": nmf_preiterations,
+                    "random_state": 0,
+                },
+                env_manager="local",
+                experiment_id=experiment_id,
+                synchronous=False,
+            )
+            succeeded = p.wait()
+            device_status[device] = False
+
+            tracking_client.set_terminated(child_run.info.run_id)
+
+            if succeeded:
+                training_run = tracking_client.get_run(p.run_id)
+                metrics = training_run.data.metrics
+                nll = metrics["nll"]
+            else:
+                tracking_client.set_terminated(p.run_id, "FAILED")
+                nll = null_nll
+
+            mlflow.log_metrics({"nll": nll})
+
+            return p.run_id, nll
+
+        return evaluate
+
+    with mlflow.start_run() as parent_run:
+        experiment_id = parent_run.info.experiment_id
+        null_evaluate = generate_evaluate_function(parent_run, 0, 0, 0, experiment_id)
+        null_hyperparameters = {
+            "K": 2,
+            "lambda_Sigma_x_inv": 0,
+            "lambda_Sigma_bar": 0,
+            "hierarchical_levels": 1,
+            "binning_downsample_rate": 0.5
+        }
+        _, null_nll = null_evaluate(null_hyperparameters)
+
+        hyperparameter_options_list = []
+        for hyperparameter_name in hyperparameter_names:
+            search_space = configuration['hyperparameters'][hyperparameter_name]
+
+            start = search_space['start']
+            end = search_space['end']
+            scale = search_space['scale']
+            gridpoints = search_space['gridpoints']
+            dtype = search_space['dtype'] if 'dtype' in search_space else 'float'
+
+            if scale == 'log':
+                gridspace = np.logspace
+            elif scale == 'linear':
+                gridspace = np.linspace
+            
+            hyperparameter_options = gridspace(start, end, num=gridpoints)
+
+            if dtype == 'int':
+                hyperparameter_options = np.round(hyperparameter_options)
+
+            hyperparameter_options_list.append(hyperparameter_options)
+
+        options = list(dict(zip(hyperparameter_names, hyperparameter_choice)) for hyperparameter_choice in itertools.product(*hyperparameter_options_list))
+
+        evaluate = generate_evaluate_function(parent_run, nmf_preiterations, spatial_preiterations, num_iterations, experiment_id, null_nll),
+        with ThreadPoolExecutor(max_workers=max_p) as executor:
+            _ = executor.map(evaluate, options,)
+
+        # find the best run, log its metrics as the final metrics of this run.
+        client = MlflowClient()
+        runs = client.search_runs(
+            [experiment_id], f"tags.mlflow.parentRunId = '{run.info.run_id}' "
+        )
+        best_nll = np.finfo(np.float64).max
+        best_run = None
+
+        nlls = [run.data.metrics["nll"] for run in runs]
+        best_nll = np.min(nlls)
+        best_run = runs[np.argmin(nlls)]
+
+        mlflow.set_tag("best_run", best_run.info.run_id)
+        mlflow.log_metrics(
+            {
+                "nll": best_nll
+            }
+        )
+
+if __name__ == "__main__":
+    run()
---
+++ popari/mlflow/util.py
@@ -0,0 +1,80 @@
+def generate_mlproject_file(project_name: str, output_filepath: str = "MLproject"):
+    """Generate MLproject file for command line use.
+
+    Args:
+        project_name: name of project
+
+    """
+
+    mlproject_contents = f"""
+    name: {project_name}
+
+    entry_points:
+      train_debug:
+        parameters:
+          dataset_path: {{type: str, default: "input.h5ad"}}
+          output_path: {{type: str, default: "output.h5ad"}} 
+          K: {{type: int}}
+          dtype: {{type: str}}
+          torch_device: {{type: str}}
+          initial_device: {{type: str}}
+          lambda_Sigma_x_inv: {{type: float, default: 1e-4}} 
+          lambda_Sigma_bar: {{type: float, default: 0}}
+          spatial_affinity_groups: {{type: str, default: 'null'}}
+          hierarchical_levels: {{type: int}}
+          binning_downsample_rate: {{type: float, default: 0.2}}
+          num_iterations: {{type: int}} 
+          spatial_preiterations: {{type: int}} 
+          nmf_preiterations: {{type: int}} 
+          random_state: {{type: int, default: 0}} 
+        command: "popari-mlflow --dataset_path={{dataset_path}} --output_path={{output_path}}
+                                     --K={{K}} --dtype={{dtype}} --torch_device={{torch_device}}
+                                     --initial_device={{initial_device}}
+                                     --lambda_Sigma_x_inv={{lambda_Sigma_x_inv}}
+                                     --lambda_Sigma_bar={{lambda_Sigma_bar}}
+                                     --spatial_affinity_groups={{spatial_affinity_groups}}
+                                     --hierarchical_levels={{hierarchical_levels}}
+                                     --binning_downsample_rate={{binning_downsample_rate}}
+                                     --num_iterations={{num_iterations}}
+                                     --spatial_preiterations={{spatial_preiterations}}
+                                     --nmf_preiterations={{nmf_preiterations}}
+                                     --random_state={{random_state}}"
+      
+      train:
+        parameters:
+          dataset_path: {{type: str, default: "input.h5ad"}}
+          output_path: {{type: str, default: "output.h5ad"}} 
+          K: {{type: int}}
+          dtype: {{type: str}}
+          torch_device: {{type: str}}
+          initial_device: {{type: str}}
+          lambda_Sigma_x_inv: {{type: float, default: 1e-4}} 
+          lambda_Sigma_bar: {{type: float, default: 0}}
+          spatial_affinity_groups: {{type: str, default: 'null'}}
+          hierarchical_levels: {{type: int}}
+          binning_downsample_rate: {{type: float, default: 0.2}}
+          num_iterations: {{type: int}} 
+          spatial_preiterations: {{type: int}} 
+          nmf_preiterations: {{type: int}} 
+          random_state: {{type: int, default: 0}} 
+        command: "popari-mlflow --dataset_path={{dataset_path}} --output_path={{output_path}}
+                                     --K={{K}} --dtype={{dtype}} --torch_device={{torch_device}}
+                                     --initial_device={{initial_device}}
+                                     --lambda_Sigma_x_inv={{lambda_Sigma_x_inv}}
+                                     --lambda_Sigma_bar={{lambda_Sigma_bar}}
+                                     --spatial_affinity_groups={{spatial_affinity_groups}}
+                                     --hierarchical_levels={{hierarchical_levels}}
+                                     --binning_downsample_rate={{binning_downsample_rate}}
+                                     --num_iterations={{num_iterations}}
+                                     --spatial_preiterations={{spatial_preiterations}}
+                                     --nmf_preiterations={{nmf_preiterations}}
+                                     --random_state={{random_state}} output.txt 2>&1"
+      
+      popari:
+        parameters:
+          configuration_filepath: path
+        command: "popari-grid-search --configuration_filepath {{configuration_filepath}}
+    """
+
+    with open(output_filepath, "w") as f:
+        f.writelines(mlproject_contents)
