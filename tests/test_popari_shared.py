064bf3c2dc7069dd6ddd5d5e2765a6fcee21bccf
Shahul Alam <alam.shahul@gmail.com>
2024-09-27 01:50:26 -0400

    Fix up shared Popari test.

--- tests/test_popari_shared.py
+++ tests/test_popari_shared.py
@@ -9,13 +9,18 @@ from sklearn.metrics import adjusted_rand_score, silhouette_score
 from sklearn.preprocessing import StandardScaler
 from tqdm.auto import tqdm, trange
 
+from popari import tl
 from popari.model import Popari
 from popari.util import clustering_louvain_nclust
 
 
 @pytest.fixture(scope="module")
-def popari_with_neighbors():
-    path2dataset = Path("tests/test_data/synthetic_dataset")
+def test_datapath():
+    return Path("tests/test_data/synthetic_dataset")
+
+
+@pytest.fixture(scope="module")
+def popari_with_neighbors(test_datapath):
     obj = Popari(
         K=10,
         lambda_Sigma_x_inv=1e-3,
@@ -23,7 +28,7 @@ def popari_with_neighbors():
         torch_context=dict(device="cuda:0", dtype=torch.float64),
         initial_context=dict(device="cuda:0", dtype=torch.float64),
         initialization_method="svd",
-        dataset_path=path2dataset / "all_data.h5",
+        dataset_path=test_datapath / "all_data.h5",
         verbose=4,
     )
 
@@ -46,13 +51,14 @@ def popari_with_neighbors():
             group_name: np.arange(4).reshape((2, 2)) for group_name in obj.metagene_groups
         }
 
-    obj.save_results(path2dataset / "trained_4_iterations.h5ad")
+    if not (test_datapath / "trained_4_iterations.h5ad").exists():
+        obj.save_results(test_datapath / "trained_4_iterations.h5ad")
+
     return obj
 
 
 @pytest.fixture(scope="module")
-def popari_with_leiden_initialization():
-    path2dataset = Path("tests/test_data/synthetic_dataset")
+def popari_with_leiden_initialization(test_datapath):
     replicate_names = [0, 1]
     obj = Popari(
         K=10,
@@ -60,7 +66,7 @@ def popari_with_leiden_initialization():
         metagene_mode="shared",
         torch_context=dict(device="cuda:0", dtype=torch.float64),
         initial_context=dict(device="cuda:0", dtype=torch.float64),
-        dataset_path=path2dataset / "all_data.h5",
+        dataset_path=test_datapath / "all_data.h5",
         replicate_names=replicate_names,
         verbose=4,
     )
@@ -70,67 +76,42 @@ def test_leiden_initialization(popari_with_leiden_initialization):
     pass
 
 
-def test_Sigma_x_inv(popari_with_neighbors):
+def test_Sigma_x_inv(popari_with_neighbors, test_datapath):
     Sigma_x_inv = (
         list(popari_with_neighbors.parameter_optimizer.spatial_affinity_state.values())[0].cpu().detach().numpy()
     )
-    np.save("tests/test_data/synthetic_dataset/outputs/Sigma_x_inv_shared.npy", Sigma_x_inv)
-    test_Sigma_x_inv = np.load("tests/test_data/synthetic_dataset/outputs/Sigma_x_inv_shared.npy")
+    # np.save("outputs/Sigma_x_inv_shared.npy", Sigma_x_inv)
+    test_Sigma_x_inv = np.load(test_datapath / "outputs/Sigma_x_inv_shared.npy")
     assert np.allclose(test_Sigma_x_inv, Sigma_x_inv)
 
 
-def test_M(popari_with_neighbors):
+def test_M(popari_with_neighbors, test_datapath):
     M_bar = popari_with_neighbors.parameter_optimizer.metagene_state.metagenes.detach().cpu().numpy()
-    np.save("tests/test_data/synthetic_dataset/outputs/M_bar_shared.npy", M_bar)
-    test_M = np.load("tests/test_data/synthetic_dataset/outputs/M_bar_shared.npy")
+    # np.save(test_datapath / "outputs/M_bar_shared.npy", M_bar)
+    test_M = np.load(test_datapath / "outputs/M_bar_shared.npy")
     assert np.allclose(test_M, M_bar)
 
 
-def test_X_0(popari_with_neighbors):
+def test_X_0(popari_with_neighbors, test_datapath):
     X_0 = popari_with_neighbors.embedding_optimizer.embedding_state["0"].detach().cpu().numpy()
-    np.save("tests/test_data/synthetic_dataset/outputs/X_0_shared.npy", X_0)
-    test_X_0 = np.load("tests/test_data/synthetic_dataset/outputs/X_0_shared.npy")
+    # np.save(test_datapath / "outputs/X_0_shared.npy", X_0)
+    test_X_0 = np.load(test_datapath / "outputs/X_0_shared.npy")
     assert np.allclose(test_X_0, X_0)
 
 
 def test_louvain_clustering(popari_with_neighbors):
-    df_meta = []
-    path2dataset = Path("tests/test_data/synthetic_dataset")
-    repli_list = [0, 1]
-    expected_aris = [0.8153973232569635, 0.8493627279334213]
-    expected_silhouettes = [0.3065145002352882, 0.3437466653435347]
+    tl.preprocess_embeddings(popari_with_neighbors, joint=True)
+    tl.leiden(popari_with_neighbors, joint=True, target_clusters=8)
+    tl.compute_ari_scores(popari_with_neighbors, labels="cell_type", predictions="leiden")
+    tl.compute_silhouette_scores(popari_with_neighbors, labels="cell_type", embeddings="normalized_X")
+    tl.evaluate_classification_task(popari_with_neighbors, labels="cell_type", embeddings="normalized_X", joint=False)
+    tl.evaluate_classification_task(popari_with_neighbors, labels="cell_type", embeddings="normalized_X", joint=True)
 
-    for index, (r, X) in enumerate(popari_with_neighbors.embedding_optimizer.embedding_state.items()):
-        #     df = pd.read_csv(path2dataset / 'files' / f'meta_{r}.csv')
-        df = pd.read_csv(path2dataset / "files" / f"celltypes_{r}.txt", header=None)
-        df.columns = ["cell type"]
-        df["repli"] = r
-        df["cell type"] = pd.Categorical(df["cell type"], categories=np.unique(df["cell type"]))
-        df_meta.append(df)
+    expected_aris = [0.7638266995561214, 0.8126115509883821]
+    for expected_ari, dataset in zip(expected_aris, popari_with_neighbors.datasets):
+        assert expected_ari == pytest.approx(dataset.uns["ari"])
 
-        x = StandardScaler().fit_transform(X.cpu().numpy())
-
-        y = clustering_louvain_nclust(
-            x.copy(),
-            8,
-            kwargs_neighbors=dict(n_neighbors=10),
-            kwargs_clustering=dict(),
-            resolution_boundaries=(0.1, 1.0),
-        )
-
-        df["label Popari"] = y
-        ari = adjusted_rand_score(*df[["cell type", "label Popari"]].values.T)
-        print(ari)
-        assert expected_aris[index] == pytest.approx(ari)
-
-        silhouette = silhouette_score(x, df["cell type"])
-        print(silhouette)
-        assert expected_silhouettes[index] == pytest.approx(silhouette)
-
-
-if __name__ == "__main__":
-    test_Sigma_x_inv(example_popari_run)
-    test_M()
-    test_X_0()
-
-    test_louvain_clustering()
+    expected_silhouettes = [0.30699036262154916, 0.34466040612221843]
+    for expected_silhouette, dataset in zip(expected_silhouettes, popari_with_neighbors.datasets):
+        print(f"Silhouette score: {dataset.uns['silhouette']}")
+        assert expected_silhouette == pytest.approx(dataset.uns["silhouette"])
