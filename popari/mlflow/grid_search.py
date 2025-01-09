feb0d07d08562819db7331d854c88c5dd9d24f61
Shahul Alam <alam.shahul@gmail.com>
2025-01-08 20:33:20 -0500

    Increase grid search verbosity.

--- popari/mlflow/grid_search.py
+++ popari/mlflow/grid_search.py
@@ -106,7 +106,7 @@ def run():
                     "spatial_affinity_mode": (
                         "shared lookup" if params["lambda_Sigma_bar"] == 0 else "differential lookup"
                     ),
-                    "verbose": 1,
+                    "verbose": 2,
                 },
                 env_manager="local",
                 experiment_id=experiment_id,
