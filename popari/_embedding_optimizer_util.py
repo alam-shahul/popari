b9c6b21052ce98c446eb8e8eecb83c178cc78bbf
Raehash Viral Shah <raehashs@oven.compbio.cs.cmu.edu>
2025-04-10 21:32:32 -0400

    Add BatchEffect Module

--- popari/_embedding_optimizer_util.py
+++ popari/_embedding_optimizer_util.py
@@ -143,8 +143,8 @@ class EmbeddingLossWithNeighbors(nn.Module, ABC):
         return f.item(), g
 
     def compute_loss(self):
-        XB = self.Z * self.S  # TODO removed batch effect, add back in
-        loss = ((XB @ self.MTM) * XB).sum() / 2 - (XB * self.YM).sum() + self.Ynorm / 2
+        X = self.Z * self.S  # TODO removed batch effect, add back in
+        loss = ((X @ self.MTM) * X).sum() / 2 - (X * self.YM).sum() + self.Ynorm / 2
         if self.prior_x_mode == "exponential shared fixed":
             loss += self.prior_x[0][0] * self.S.sum()
         elif not self.prior_x_mode:
@@ -286,3 +286,117 @@ class EmbeddingLossWithNeighborsNesterov(EmbeddingLossWithNeighbors):
 #         return
 #
 #     return update_s()
+
+
+class BatchEffectEmbeddingLossWithNeighborsNesterov(EmbeddingLossWithNeighbors):
+
+    def update_s(self, B):
+        # S[:] = (YM * Z).sum(axis=1, keepdim=True)
+        self.S[:] = (YM * Z - ((Z @ MTM) * B)).sum(axis=1, keepdim=True)
+
+        if self.prior_x_mode == "exponential shared fixed":
+            # TODO: why divide by two?
+            self.S.sub_(self.prior_x[0][0] / 2)
+        elif not self.prior_x_mode:
+            pass
+        else:
+            raise NotImplementedError
+
+        denominator = ((self.Z @ self.MTM) * self.Z).sum(axis=1, keepdim=True)
+        self.S.div_(denominator)
+        self.S.clip_(min=1e-5)
+
+    def compute_loss(self, B):
+        XB = self.Z * self.S + B
+        loss = ((XB @ self.MTM) * XB).sum() / 2 - (XB * self.YM).sum() + self.Ynorm / 2
+        if self.prior_x_mode == "exponential shared fixed":
+            loss += self.prior_x[0][0] * self.S.sum()
+        elif not self.prior_x_mode:
+            pass
+        else:
+            raise NotImplementedError
+
+        if self.Sigma_x_inv is not None:
+            loss += ((self.adjacency_matrix @ self.Z) @ self.Sigma_x_inv).mul(self.Z).sum() / 2
+        loss = loss.item()
+        # assert loss <= loss_prev, (loss_prev, loss)
+
+        return loss
+
+    # Same as EmbeddingLossWithNeighborsNesterov
+    def update_z(self, Z):
+        pbar = trange(self.N, leave=False, disable=True, desc="Updating Z w/ nbrs via Nesterov GD")
+        func, grad = self.get_batch_loss_and_grad(
+            Z,
+            self.S,
+            self.MTM,
+            self.YM * self.S - self.adjacency_matrix @ Z @ self.Sigma_x_inv / 2,
+        )
+        for idx in IndependentSet(self.E_adjacency_list, device=self.device, batch_size=1024):
+            quad_batch = self.MTM
+            linear_batch_spatial = -torch.index_select(self.adjacency_matrix, 0, idx) @ Z @ self.Sigma_x_inv
+            Z_batch = Z[idx].contiguous()
+            S_batch = self.S[idx].contiguous()
+            optimizer = NesterovGD(Z_batch, self.base_step_size / S_batch.square())
+            ppbar = trange(100, leave=False, disable=not (self.verbose > 3))
+            for i_iter in ppbar:
+                if self.embedding_acceleration_trick:
+                    self.update_s()  # TODO: update S_batch directly
+                S_batch = self.S[idx].contiguous()
+                linear_batch = linear_batch_spatial + self.YM[idx] * S_batch
+                if i_iter == 0:
+                    func, grad = self.get_batch_loss_and_grad(
+                        Z_batch,
+                        S_batch,
+                        quad_batch,
+                        linear_batch,
+                    )  # TODO: if we remove this line does it still run?
+                    func, grad = self.get_batch_loss_and_grad(
+                        Z,
+                        self.S,
+                        self.MTM,
+                        self.YM * self.S - self.adjacency_matrix @ Z @ self.Sigma_x_inv / 2,
+                    )
+
+                NesterovGD.step_size = (
+                    self.base_step_size / S_batch.square()
+                )  # TM: I think this converges as s converges
+                func, grad = self.get_batch_loss_and_grad(Z_batch, S_batch, quad_batch, linear_batch)
+
+                Z_batch_prev = Z_batch.clone()
+                Z_batch = optimizer.step(grad)
+
+                if self.use_inplace_ops:
+                    Z_batch = project2simplex_(Z_batch, dim=1)
+                else:
+                    Z_batch = project2simplex(Z_batch, dim=1)
+
+                optimizer.set_parameters(Z_batch)
+
+                dZ = (Z_batch_prev - Z_batch).abs().max().item()
+                Z[idx] = Z_batch
+                description = f"func={func:.1e}, dZ={dZ:.1e}"
+                ppbar.set_description(description)
+                if dZ < self.tol:
+                    break
+            ppbar.close()
+
+            Z[idx] = Z_batch
+            func, grad = self.get_batch_loss_and_grad(Z_batch, S_batch, quad_batch, linear_batch)
+            func, grad = self.get_batch_loss_and_grad(
+                Z,
+                self.S,
+                self.MTM,
+                self.YM * self.S - self.adjacency_matrix @ Z @ self.Sigma_x_inv / 2,
+            )
+            pbar.update(len(idx))
+
+        pbar.close()
+        func, grad = self.get_batch_loss_and_grad(
+            Z,
+            self.S,
+            self.MTM,
+            self.YM * self.S - self.adjacency_matrix @ Z @ self.Sigma_x_inv / 2,
+        )
+
+        return Z
