add exponential shared fixed prior to cross dataset average prior

popari/_embedding_optimizer.py
@@ -115,9 +115,10 @@ class EmbeddingOptimizer:
                     if other_dataset.name != dataset.name:
                         embeddings_list.append(self.embedding_state[dataset.name])
                 stacked_embeddings = torch.cat(embeddings_list, dim=0)
+                # stacked_embeddings = torch.stack(embeddings_list, dim=0)
                 prior_embeddings = torch.mean(stacked_embeddings, dim=0)
                 average_across_samples.append(prior_embeddings)
-                print("priors", prior_embeddings[0])
+                print("priors", prior_embeddings)
 
         for dataset_index, dataset in enumerate(self.datasets):
             is_spatial_replicate = "adjacency_list" in dataset.obsm

popari/_embedding_optimizer_util.py
@@ -79,9 +79,10 @@ class EmbeddingLossTripletLossNoNeighborsGD(EmbeddingLossNoNeighbors):
         elif self.prior_x_mode == "cross_dataset_average":
             sign_of_prior = torch.sign(X - self.average_across_samples)
             # print("sign of prior", linear_term_gradient.shape, sign_of_prior.shape)
-            linear_term_gradient = linear_term_gradient - (self.prior_x[0][None] * sign_of_prior)
             # linear_term_gradient = linear_term_gradient - (self.prior_x[0][None] * sign_of_prior)
-            # linear_term_gradient = linear_term_gradient - ((self.prior_x[0][None] * sign_of_prior) + self.prior_x[0][None])
+            linear_term_gradient = linear_term_gradient - (
+                (self.prior_x[0][None] * sign_of_prior) + self.prior_x[0][None]
+            )
         elif not self.prior_x_mode:
             pass
         else:
@@ -532,9 +533,8 @@ class BatchEffectTripletLossEmbeddingLossWithNeighborsNesterov(BatchEffectEmbedd
                 axis=1,
                 keepdim=True,
             )
-            # print("sign of prior", self.S.shape, sign_of_prior.shape)
-            self.S.sub_(self.prior_x[0][0] * sign_of_prior / 2)
-            # self.S.sub_((self.prior_x[0][0] * sign_of_prior + self.prior_x[0][0])/ 2)
+            # self.S.sub_(self.prior_x[0][0] * sign_of_prior / 2)
+            self.S.sub_((self.prior_x[0][0] * sign_of_prior + self.prior_x[0][0]) / 2)
         elif not self.prior_x_mode:
             pass
         else:
@@ -550,8 +550,11 @@ class BatchEffectTripletLossEmbeddingLossWithNeighborsNesterov(BatchEffectEmbedd
         if self.prior_x_mode == "exponential shared fixed":
             loss += self.prior_x[0][0] * self.S.sum()
         elif self.prior_x_mode == "cross_dataset_average":
-            loss += self.prior_x[0][0] * torch.abs(self.S - self.average_across_samples).sum()
-            # loss += (self.prior_x[0][0] * torch.abs(self.S - self.average_across_samples).sum() + self.prior_x[0][0] * self.S.sum())
+            # loss += self.prior_x[0][0] * torch.abs(self.S - self.average_across_samples).sum()
+            loss += (
+                self.prior_x[0][0] * torch.abs(self.S - self.average_across_samples).sum()
+                + self.prior_x[0][0] * self.S.sum()
+            )
         elif not self.prior_x_mode:
             pass
         else:

popari/_parameter_optimizer.py
@@ -117,7 +117,7 @@ class ParameterOptimizer:
         if all(prior_x_mode == "exponential shared fixed" for prior_x_mode in self.prior_x_modes):
             self.prior_xs = [(torch.ones(self.K, **self.initial_context),) for _ in range(len(self.datasets))]
         elif all(prior_x_mode == "cross_dataset_average" for prior_x_mode in self.prior_x_modes):
-            self.prior_xs = [(0.7 * torch.ones(self.K, **self.initial_context),) for _ in range(len(self.datasets))]
+            self.prior_xs = [(torch.ones(self.K, **self.initial_context),) for _ in range(len(self.datasets))]
         elif all(prior_x_mode == None for prior_x_mode in self.prior_x_modes):
             self.prior_xs = [(torch.zeros(self.K, **self.initial_context),) for _ in range(len(self.datasets))]
         else: